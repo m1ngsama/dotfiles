8caf78b02108cddf317118ab312132f1da4f00f2
m1ngsama <contact@m1ng.space>
2025-09-26 16:47:38 +0800

    add some alias from test machine

--- fish/config.fish
+++ fish/config.fish
@@ -18,12 +18,38 @@ set -Ux PATH $PATH $GOPATH/bin
 thefuck --alias | source
 
 function fish_greeting
-echo 'Talk is cheap. Show me the code.' | lolcat -a -d 10
+    echo 'Talk is cheap. Show me the code.' | lolcat -a -d 10
 end
 
 # pnpm
-set -gx PNPM_HOME "/Users/m1ng/Library/pnpm"
+set -gx PNPM_HOME /Users/m1ng/Library/pnpm
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
+
+## move from arch
+##function fish_greeting
+#end
+#
+#alias cman="man -L zh_CN"
+#alias all="eza -alh --icons --group-directories-first --time-style=long-iso --total-size"
+#
+#set -x ALL_PROXY "socks5h://127.0.0.1:1080"
+#set -x http_proxy "socks5h://127.0.0.1:1080"
+#set -x https_proxy "socks5h://127.0.0.1:1080"
+#
+## set -x http_proxy "http://127.0.0.1:8118"
+## set -x https_proxy "https://127.0.0.1:8118"
+## set -x ALL_PROXY "https://127.0.0.1:8118"
+#
+### if not set -q NODE_VERSION
+###    nvm use lts
+### end
+#
+## pnpm
+#set -gx PNPM_HOME "/home/m1ng/.local/share/pnpm"
+#if not string match -q -- $PNPM_HOME $PATH
+#    set -gx PATH "$PNPM_HOME" $PATH
+#end
+## pnpm end
