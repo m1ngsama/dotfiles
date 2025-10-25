cce1d7df67f2ea207ebd3fa034cca4d0c3e83b4b
m1ngsama <contact@m1ng.space>
2025-10-25 18:01:08 +0800

    update some comment

--- fish/config.fish
+++ fish/config.fish
@@ -21,7 +21,7 @@ function fish_greeting
     echo 'Talk is cheap. Show me the code.' | lolcat -a -d 10
 end
 
-# pnpm
+# pnpm in mac
 set -gx PNPM_HOME /Users/m1ng/Library/pnpm
 if not string match -q -- $PNPM_HOME $PATH
     set -gx PATH "$PNPM_HOME" $PATH
@@ -31,25 +31,28 @@ end
 ## move from arch
 ##function fish_greeting
 #end
-#
+
+# Man in zh_CN, you should install man-pages-zh_cn manual first
 #alias cman="man -L zh_CN"
 #alias all="eza -alh --icons --group-directories-first --time-style=long-iso --total-size"
-#
-#set -x ALL_PROXY "socks5h://127.0.0.1:1080"
-#set -x http_proxy "socks5h://127.0.0.1:1080"
-#set -x https_proxy "socks5h://127.0.0.1:1080"
-#
+
+# When use shadowsocks as proxy you need that
+## set -x ALL_PROXY "socks5h://127.0.0.1:1080"
+## set -x http_proxy "socks5h://127.0.0.1:1080"
+## set -x https_proxy "socks5h://127.0.0.1:1080"
 ## set -x http_proxy "http://127.0.0.1:8118"
+
+# Privoxy, filtering proxy for the HTTP protocol
 ## set -x https_proxy "https://127.0.0.1:8118"
 ## set -x ALL_PROXY "https://127.0.0.1:8118"
-#
+
 ### if not set -q NODE_VERSION
 ###    nvm use lts
 ### end
-#
-## pnpm
-#set -gx PNPM_HOME "/home/m1ng/.local/share/pnpm"
-#if not string match -q -- $PNPM_HOME $PATH
-#    set -gx PATH "$PNPM_HOME" $PATH
-#end
+
+## pnpm in arch
+# set -gx PNPM_HOME "/home/m1ng/.local/share/pnpm"
+# if not string match -q -- $PNPM_HOME $PATH
+# set -gx PATH "$PNPM_HOME" $PATH
+# end
 ## pnpm end
